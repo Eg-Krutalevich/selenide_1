ea56014d1d06a338b4fee39be3b54f6ba1c766ef
Круталевич Егор <@e.krutalevich_pflb>
2024-04-27 18:38:07 +0300

    New1

--- src/test/java/tests/AppleTest.java
+++ src/test/java/tests/AppleTest.java
@@ -18,5 +18,15 @@ public class AppleTest extends BaseTest {
         String href = searchPage.getHrefFromFirstArticle();
         Assert.assertTrue(href.contains(TestConst.EXPECTED_WORD));
     }
+    @Test
+    public void checkHref1() {
+        AppleMainPage mainPage = new AppleMainPage(TestConst.URL_APPLE);
+        mainPage.search(TestConst.SEARCH_STRING);
+
+        SearchPage searchPage = new SearchPage();
+        String href = searchPage.getHrefFromFirstArticle();
+        Assert.assertTrue(href.contains(TestConst.EXPECTED_WORD));
+    }
+
 
 }
